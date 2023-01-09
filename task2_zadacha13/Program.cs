091b4d5463a2f5abba7b3fccb1c2594028d61228
Daria <dorothyfedorova@gmail.com>
2023-01-09 20:26:31 +0300

    create solution of task13

--- task2_zadacha13/Program.cs
+++ task2_zadacha13/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿/* // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+*/
+
+/* Задача 13: Напишите программу, которая 
+выводит третью цифру заданного числа или сообщает, 
+что третьей цифры нет.
+
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+Console.Clear();
+Console.WriteLine("введите число");
+
+int number = int.Parse(Console.ReadLine());
+
+if (number < 100)
+{
+    Console.WriteLine("введенное число имеет менее трех цифр");
+}
+else{
+    while (number > 1000)
+    {
+        number = number / 10;
+    }
+    Console.WriteLine($"третья цифра введенного числа = {number % 10}");
+}
