776d2926689b48339afa794c5567569914b3f246
Daria <dorothyfedorova@gmail.com>
2023-01-09 20:56:03 +0300

    add solution of task15

--- task3_zadacha15/Program.cs
+++ task3_zadacha15/Program.cs
@@ -10,6 +10,24 @@ Console.WriteLine("Hello, World!");
 1 -> нет
 */
 
+Console.Clear();
+Console.WriteLine("введите число от 1 до 7, где 1=понедельник, 7=воскресенье");
+int number = int.Parse(Console.ReadLine());
 
+if (number < 1 || number > 7)
+{
+    Console.WriteLine("введите число от 1 до 7");
+}
+else
+{
+    if (number == 6 || number == 7)
+    {
+        Console.WriteLine("это выходной день недели");
+    }
+    else
+    {
+        Console.WriteLine("этот день является будним");
+    }
+}
 
 
